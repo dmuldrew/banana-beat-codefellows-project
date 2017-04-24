2389c6f00eafd7ab38e044a36e536ca538df89be
Joshua Evans <skyfriends1@gmail.com>
2017-04-24 15:09:55 -0700

    tables ppears, boxes change colors, it works!

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 
-var bpm = 10;
+var bpm = 80;
 var currentBeat = 0;
 
 function Drum(name, sample){
@@ -15,20 +15,48 @@ Drum.prototype.playDrum = function(){
   new Audio(this.sample).play();
 };
 
+function generateTable(allDrums) {
+  for(var i = 0; i < allDrums.length; i++) {
+    generateRow(allDrums[i]);
+  }
+}
+
+function generateRow(drum) {
+  var table = document.getElementById('grid-beat');
+  var row = document.createElement('tr');
+  var drumName = document.createElement('td');
+  drumName.textContent = drum.name;
+  row.appendChild(drumName);
+  var beatBox;
+  for (var i = 0; i < drum.playTriggers.length; i++) {
+    beatBox = document.createElement('td');
+    beatBox.className = 'off';
+    beatBox.setAttribute('count-index', i);
+    row.appendChild(beatBox);
+    beatBox.addEventListener('click', function(e) {toggleTrigger(e, drum);});
+  }
+  table.appendChild(row);
+}
+
+function toggleTrigger(e, drum) {
+  var beatBox = e.target;
+  if (beatBox.className === 'off') {
+    drum.playTriggers[beatBox.getAttribute('count-index')] = true;
+    beatBox.style.background = 'blue';
+    beatBox.className = 'on';
+  } else {
+    drum.playTriggers[beatBox.getAttribute('count-index')] = false;
+    beatBox.style.background = 'transparent';
+    beatBox.className = 'off';
+  }
+}
+
 var snare = new Drum('snare', 'Samples/snare-big.mp3');
 var hihat = new Drum('hihat', 'Samples/hihat-808.mp3');
 
 var allDrums = [snare, hihat];
 
-snare.playTriggers[1] = true;
-snare.playTriggers[2] = true;
-hihat.playTriggers[5] = true;
-hihat.playTriggers[7] = true;
-snare.playTriggers[9] = true;
-snare.playTriggers[11] = true;
-hihat.playTriggers[13] = true;
-hihat.playTriggers[15] = true;
-
+generateTable(allDrums);
 
 setInterval(playBeat, 6000 / bpm);
 
@@ -36,6 +64,14 @@ function playBeat(){
   for (var i = 0; i < allDrums.length; i++){
     if (allDrums[i].playTriggers[currentBeat]){
       allDrums[i].playDrum();
+    }
+  }
+  var allBoxes = document.getElementsByTagName('td');
+  for (i = 0; i < allBoxes.length; i++) {
+    if (allBoxes[i].getAttribute('count-index') == currentBeat) {
+      allBoxes[i].style.borderColor = 'red';
+    } else {
+      allBoxes[i].style.borderColor = 'black';
     }
   }
   currentBeat++;
